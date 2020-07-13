a9a6f01b4f11e9b1a3b76af16f8002c99552392d
Zinadin Zidan <zidan44@pixelexperience.org>
2020-07-13 11:32:00 +0600

    device: mtk: sepolicy: Import Initial Common Sepolicy Rules
    * Rules Are Taken From MTK Alpha
    * Updated Some Rules To Match With Source
    
    Change-Id: I2d559a0f5c5326e9da0faf66a300abf6e79c1b3f

---
+++ sepolicy.mk
@@ -0,0 +1,15 @@
+BOARD_SEPOLICY_DIRS += \
+    device/mediatek/sepolicy/basic/non_plat \
+    device/mediatek/sepolicy/bsp/non_plat \
+    device/mediatek/sepolicy/bsp/non_plat/ota \
+    device/mediatek/sepolicy/full/non_plat
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/plat_public \
+    device/mediatek/sepolicy/bsp/plat_public \
+    device/mediatek/sepolicy/full/plat_public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/plat_private \
+    device/mediatek/sepolicy/bsp/plat_private \
+    device/mediatek/sepolicy/full/plat_private
