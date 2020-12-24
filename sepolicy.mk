249759bf005b81b612916816624fe1934d00af94
Aayush Gupta <aayushgupta219@gmail.com>
2020-12-24 16:56:51 +0530

    mediatek: Inherit lineage sepcific mediatek sepolicy
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I9918ae0ce11bb41a6081d3137591d3626d337a74

--- sepolicy.mk
+++ sepolicy.mk
@@ -7,3 +7,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
         device/mediatek/sepolicy/plat_private
+
+# Inherit lineage sepolicy for mediatek devices
+-include device/lineage/sepolicy/mediatek/sepolicy.mk
