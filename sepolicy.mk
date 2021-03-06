a0b3b829d974574f680f5e5f5b59f55a275599fc
Zinadin Zidan <zidan44@pixelexperience.org>
2021-03-06 09:36:22 +0600

    sepolicy: Initial plat public sepolicy rules
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I71117737ca00b176b72dc741835802d7fe7c11c8

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,5 +1,10 @@
 # Board specific SELinux policy variable definitions
 
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/plat_public \
+    device/mediatek/sepolicy/bsp/plat_public \
+    device/mediatek/sepolicy/full/plat_public
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/mediatek/sepolicy/basic/plat_private \
     device/mediatek/sepolicy/bsp/plat_private \
