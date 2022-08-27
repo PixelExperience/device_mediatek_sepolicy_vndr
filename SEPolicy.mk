9345b8831219396bc4b205217617f99ec2674475
Zinadin Zidan <zidan44@pixelexperience.org>
2022-08-28 02:08:21 +0600

    sepolicy: Sync with latest mtk a12 sepolicy
    * From: https://github.com/Teracube-Inc/platform_device_mediatek_sepolicy
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Idfe5227f1f83159480e89da25e02dfa65eadc553

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -4,18 +4,19 @@ MTK_SEPOLICY_PATH:= device/mediatek/sepolicy_vndr
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(MTK_SEPOLICY_PATH)/basic/non_plat \
     $(MTK_SEPOLICY_PATH)/bsp/non_plat \
-    $(MTK_SEPOLICY_PATH)/bsp/non_plat/ota \
-    $(MTK_SEPOLICY_PATH)/full/non_plat
+    $(MTK_SEPOLICY_PATH)/modem
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     $(MTK_SEPOLICY_PATH)/basic/plat_public \
-    $(MTK_SEPOLICY_PATH)/bsp/plat_public \
-    $(MTK_SEPOLICY_PATH)/full/plat_public
+    $(MTK_SEPOLICY_PATH)/bsp/plat_public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     $(MTK_SEPOLICY_PATH)/basic/plat_private \
-    $(MTK_SEPOLICY_PATH)/bsp/plat_private \
-    $(MTK_SEPOLICY_PATH)/full/plat_private
+    $(MTK_SEPOLICY_PATH)/bsp/plat_private
+
+ifneq ($(call math_lt,$(PRODUCT_SHIPPING_API_LEVEL),28),)
+BOARD_SEPOLICY_DIRS += $(wildcard device/mediatek/sepolicy_vndr/bsp/ota_upgrade)
+endif
 
 # Inherit Custom sepolicy for mediatek devices
 -include device/custom/sepolicy/mediatek/sepolicy.mk
